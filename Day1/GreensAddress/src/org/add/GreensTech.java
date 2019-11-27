c9f5b2ce1df24187a6e18f8e8a3a0e22569147f9
Siva J <siva94j@gmail.com>
2019-11-27 23:55:53 +0530

    Redid all things in my laptop

---
+++ Day1/GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.add;
+
+/**
+ *
+ * @author siva9
+ */
+public class GreensTech {
+    public void greensOmr(){
+        System.out.println("I am at Greens OMR");
+    }
+    public void greensAdayar(){
+        System.out.println("I am at Greens Adayar");
+    }
+    public void greensTambaram(){
+        System.out.println("I am at Greens Tambaram");
+    }
+    public void greensVelacherry(){
+        System.out.println("I am at Greens Velacherry");
+    }
+    public void greensAnnaNagar(){
+        System.out.println("I am at Greens AnnaNagar");
+    }
+    public static void main(String[] args) {
+        GreensTech obj = new GreensTech();
+        obj.greensOmr();
+        obj.greensAdayar();
+        obj.greensTambaram();
+        obj.greensVelacherry();
+        obj.greensAnnaNagar();
+    }
+    
+}
